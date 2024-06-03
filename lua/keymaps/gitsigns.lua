e6c6e5563d534d83714df97551875a7a4f7df7a3
atbuy <filip1253@gmail.com>
2024-06-04 00:20:23 +0300

    Added hunk navigation for gitsigns

--- lua/keymaps/gitsigns.lua
+++ lua/keymaps/gitsigns.lua
@@ -7,8 +7,30 @@ local opts = {
 
 local keymap = vim.keymap.set
 
+function next_hunk()
+  if vim.wo.diff then
+    vim.cmd.normal({ "]c", bang = true })
+  else
+    gitsigns.nav_hunk("next")
+  end
+end
+
+function previous_hunk()
+  if vim.wo.diff then
+    vim.cmd.normal({ "[c", bang = true })
+  else
+    gitsigns.nav_hunk("prev")
+  end
+end
+
 opts.desc = "Git blame line"
 keymap("n", "gb", gitsigns.blame_line, opts)
 
 opts.desc = "Toggle deleted lines"
 keymap("n", "td", gitsigns.toggle_deleted, opts)
+
+opts.desc = "Go to next hunk"
+keymap("n", "]c", next_hunk, opts)
+
+opts.desc = "Go to previous hunk"
+keymap("n", "[c", previous_hunk, opts)
