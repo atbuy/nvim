afd13bc5c5d0ff371d900d89d81ab97f3c9a3eda
atbuy <buy@atbuy.dev>
2024-12-03 23:10:12 +0200

    Changed editor theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,18 +1,18 @@
 return {
   -- "navarasu/onedark.nvim",
   -- "olimorris/onedarkpro.nvim",
-  "catppuccin/nvim",
+  -- "catppuccin/nvim",
   -- "rebelot/kanagawa.nvim",
-  -- "marko-cerovac/material.nvim",
+  "marko-cerovac/material.nvim",
   -- "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   config = function()
     -- vim.cmd("colorscheme onedark")
-    vim.cmd("colorscheme catppuccin-frappe")
+    -- vim.cmd("colorscheme catppuccin-frappe")
     -- vim.cmd("colorscheme kanagawa")
-    -- vim.cmd("colorscheme material-darker")
+    vim.cmd("colorscheme material-darker")
     -- vim.cmd("colorscheme tokyonight-storm")
-    vim.cmd("highlight TabLineSel guifg=#aed5fc")
+    -- vim.cmd("highlight TabLineSel guifg=#aed5fc")
   end,
 }
